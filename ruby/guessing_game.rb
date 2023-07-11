ce18d42c980a22614d60eb3820c7d93f0285c48f
Gülsah <73911627+GulsahDurmaz@users.noreply.github.com>
2023-07-11 23:13:13 +0200

    added the guessing game

---
+++ ruby/guessing_game.rb
@@ -0,0 +1,18 @@
+random_number = rand(10)
+
+puts "I have picked a random between 1 and 10."
+puts "Can you guess the number?"
+
+loop do
+    print "Enter your guess: "
+    guess = gets.chomp.to_i
+
+    if guess == random_number
+        puts "Congratulations! You guessed the correct number!"
+        break
+    elsif guess < random_number
+        puts "Too low! Try again."
+    else
+        puts "Too high! Try again."
+    end
+end
---
+++ ruby/ruby.rb
@@ -0,0 +1,30 @@
+#day 1
+puts "Hello World!"
+
+properties = ['object oriented', 'duck typed', 'productive', 'fun']
+properties.each {|property| puts "Ruby is #{property}."}
+
+puts 'This appears to be true.' if 0 #0 is true!
+
+def add_them_up
+    4 + 4.0
+end
+
+i = 0
+a = ['100', 100.0]
+while i < 2
+    puts a[i].to_i #converts each to an integer
+    i = i + 1
+end
+
+#homework
+puts "Hello World."
+
+str1 = "Hello, Ruby.\n"
+puts str1.index("Ruby")
+puts str1[7, 5]
+puts str1 * 7
+
+(1..10).each do |number|
+    puts "This is sentence number #{number}."
+end
